e7fbe448adf7431c0f4e36ace5a3fa57b8cd6486
Sejal <sejaljain2012000.sj@gmail.com>
2024-01-08 01:11:32 +0530

    fix resetpassword

--- src/Components/Auth/Restpassword.jsx
+++ src/Components/Auth/Restpassword.jsx
@@ -2,11 +2,12 @@ import { Button, Container, Heading, Input, VStack } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { resetPassword } from '../../Redux/actions/userAction';
+import { useDispatch } from 'react-redux';
 
 const Restpassword = () => {
   const [password,setPassword]=useState("");
   const token = useParams();
-
+  const dispatch = useDispatch()
   const submitHandler = e =>{
     e.preventDefault();
     dispatch(resetPassword(password,token));
